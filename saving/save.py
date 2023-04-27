115560eaa64281248c11cbbd2939ebd623e72ec9
Iréné <amieheessomba@etu.unistra.fr>
2023-04-27 19:47:02 +0200

    updating

--- saving/save.py
+++ saving/save.py
@@ -6,6 +6,7 @@ from fileType       import fileType as FT
 from header         import header, formating
 from saving         import writing 
 import sys
+import platform
 
 def bottom(max_x :int = 0, color : str = "white"):
     asc         = frame.frame(custom=True)
@@ -44,6 +45,8 @@ class saveData:
         self.init               = init.init
         # Scroll display down and up
         self.scr                = scroll.scrolled
+        # system
+        self.system             = platform.system()
         
     def build(self, x: int = 0, y : int = 0, lang : str = "", WRITE : list = [], history : dict = {}):
         sys.stdout.write(
@@ -101,136 +104,177 @@ class saveData:
         self._X_, self._y_              = screenConfig.cursor()
         
         while True:
-            self.char = input.convert() 
-            # <enter> is pressed 
-            if self.char in {10, 13}:
-                self.max_x  = self._max_x_
-                for i in range(2):
-                    sys.stdout.write(self.move.LEFT(pos=1000))
-                    sys.stdout.write(
-                        _input_ + self.c_bg + " " * (self.max_x - (_length_+2) ) + self.init.reset + 
-                        self.move.TO(x=self.max_x, y=self.Y) + self.c + f"{self.acs['v']}" +
-                        self.move.TO(x=x, y=self.Y) + self.init.reset + "\n"
-                        )
-                    self.Y += 1
-                sys.stdout.write(
-                    self.move.DOWN(1)+
-                    self.move.LEFT(pos=1000)
-                    )
-                header.bottom(max_x=self.max_x)
-                sys.stdout.write(self.move.TO(x=x, y=y))
+            
+            try:
+                self.char = input.convert() 
                 
-                self.dataBase["FileName"], self.dataBase["action"]  = self.string, True
-                
-                self.language_type = FT.file(self.string)
-                if self.language_type is None: 
-                    writing.writeInput(self.dataBase["data"], self.string)
-                else: 
-                    writing.writeInput(self.dataBase["data"], self.string)
-                    if self.dataBase["data"]:
-                        WRITE       = writing.BLACK_M(self.dataBase["data"], self.language_type, history)
+                if self.char:
+                    if self.system in {"Linux", "MacOS"} : pass 
+                    elif self.system == "Windows" :
+                        _           = self.char[1]
+                        self.char   = self.char[0]
+                    else: self.char = None
+                    
+                    if self.char:
+                        # <enter> is pressed 
+                        if self.char in {10, 13}    :
+                            self.max_x  = self._max_x_
+                            for i in range(2):
+                                sys.stdout.write(self.move.LEFT(pos=1000))
+                                sys.stdout.write(
+                                    _input_ + self.c_bg + " " * (self.max_x - (_length_+2) ) + self.init.reset + 
+                                    self.move.TO(x=self.max_x, y=self.Y) + self.c + f"{self.acs['v']}" +
+                                    self.move.TO(x=x, y=self.Y) + self.init.reset + "\n"
+                                    )
+                                self.Y += 1
+                            sys.stdout.write(
+                                self.move.DOWN(1)+
+                                self.move.LEFT(pos=1000)
+                                )
+                            header.bottom(max_x=self.max_x)
+                            sys.stdout.write(self.move.TO(x=x, y=y))
+                            
+                            self.dataBase["FileName"], self.dataBase["action"]  = self.string, True
+                            
+                            self.language_type = FT.file(self.string)
+                            if self.language_type is None: 
+                                writing.writeInput(self.dataBase["data"], self.string)
+                            else: 
+                                writing.writeInput(self.dataBase["data"], self.string)
+                                if self.dataBase["data"]:
+                                    WRITE       = writing.BLACK_M(self.dataBase["data"], self.language_type, history)
+                                    sys.stdout.write(
+                                        self.move.TO(x=0, y=LINE)+ 
+                                        self.move.LEFT(1000)
+                                        )
+                                    formating.scrollUP(WRITE, self.max_x, 0, self.max_y, LINE)
+                                    sys.stdout.write(self.move.TO(x=x, y=y))
+                                else: pass
+                            break
+                        # <ctrl+c> is pressed 
+                        elif self.char == 3         :
+                            self.max_x  = self._max_x_
+                            for i in range(2):
+                                sys.stdout.write(self.move.LEFT(pos=1000))
+                                sys.stdout.write(
+                                    _input_ + self.c_bg + " " * (self.max_x - (_length_+2) ) + self.init.reset + 
+                                    self.move.TO(x=self.max_x, y=self.Y) + self.c + f"{self.acs['v']}" +
+                                    self.move.TO(x=x, y=self.Y) + self.init.reset + "\n"
+                                    )
+                                self.Y += 1
+                            sys.stdout.write(
+                                self.move.DOWN(1)+
+                                self.move.LEFT(pos=1000)
+                                )
+                            header.bottom(max_x=self.max_x)
+                            sys.stdout.write(self.move.TO(x=x, y=y))
+                            sys.stdout.flush()
+                            
+                            break
+                        # moving cursor up, down, left, reight
+                        elif self.char == 27        :
+                            if self.system in {"Linux", "MacOs"}: 
+                                next1 = input.convert()
+                                if   next1 == 91:  next2 = ord(sys.stdin.read(1)) 
+                                else: next2 = None
+                            elif self.system == "Windows"       :
+                                if type(_) is type(list()): next2 = _[0]
+                                else: next2 = _
+                            else: next2 = None 
+                            
+                            if next2 in {68, 67}:
+                                # move left 
+                                if   next2 == 68:
+                                    if self._x_>= self.length: self._x_ -= 1
+                                    else: pass 
+                                    
+                                    # scroll horizontally string  
+                                    if self._x_ == self.length -1: 
+                                        if self.counterL > 0: 
+                                            self.counterL -= 1
+                                            self.counterR -= 1
+                                        else: pass
+                                    else: pass
+                                #move right
+                                elif next2 == 67:
+                                    if self.counterL == 0:
+                                        if self._x_ < ( self.length + self.i) : self._x_ += 1
+                                        else: pass 
+                                    else:    
+                                        if self._x_< self.max_x - 3:  self._x_ += 1
+                                        else: pass 
+                                    
+                                    if self._x_ == (self.max_x-2):
+                                        # scroll horizontally string  
+                                        if self.counterR < len(self.string) : 
+                                            self.counterL += 1
+                                            self.counterR += 1
+                                        else: pass
+                                    else: pass 
+                                # else pass   
+                                else: pass
+                            else: pass
+                        # delecting char <backspace>
+                        elif self.char in {8, 127}  :
+                            if self.i > 0: 
+                                self.string = self.string[ : self.i -1] + self.string[self.i : ]
+                                self.i          -= 1
+                                self._x_        -= 1
+                                self.counterL    = 0
+                                self.counterR   -= 1
+                            else: pass
+                        # another key is pressed 
+                        else: 
+                            if  self.i < self.max_x - (2 + self.length+1):
+                                if chr(self.char) in case() :
+                                    self.string              = self.string[ : self.i] + chr(self.char) + self.string[self.i : ]
+                                    self.i                  += 1 
+                                    self.counterR            = len(self.string)
+                                    self.counterL            = 0
+                                    self._x_, self._y_       = screenConfig.cursor()
+                                else: pass
+                            else: 
+                                if chr(self.char) in case() :
+                                    self.string              = self.string[ : self.i] + chr(self.char) + self.string[self.i : ]
+                                    self.i                  += 1 
+                                    self.counterL           += 1
+                                    self.counterR            = len(self.string)
+                                else: pass
+                        
+                        sys.stdout.write(self.move.LEFT(pos=1000))
+                        sys.stdout.write(self.clear.line(2))
                         sys.stdout.write(
-                            self.move.TO(x=0, y=LINE)+ 
-                            self.move.LEFT(1000)
+                            self.input + self.c_bg + " " * ( self.max_x - (self.length+2) ) + self.init.reset + 
+                            self.move.TO(x=self.max_x, y=self._y_) + self.c + f"{self.acs['v']}" +
+                            self.move.TO(x=self.length+1, y=self._y_) + self.c_bg+self.string[self.counterL : self.counterR]  + self.init.reset +"\n"
                             )
-                        formating.scrollUP(WRITE, self.max_x, 0, self.max_y, LINE)
-                        sys.stdout.write(self.move.TO(x=x, y=y))
+                        sys.stdout.write(self.clear.screen(0))
+                        sys.stdout.write(self.move.LEFT(pos=1000))
+                        bottom(max_x=self.max_x)
+                        sys.stdout.write(self.move.TO(x=self.length+1, y=self._y_))
+                        
+                        if self.i > 0:  sys.stdout.write(self.move.RIGHT(pos=(self._x_-self.length)) )
+                        else: pass 
+                        
+                        sys.stdout.flush()
                     else: pass
-                break
-            # <ctrl+c> is pressed 
-            elif self.char == 3:
+                else: pass
+                
+            except KeyboardInterrupt: 
                 self.max_x  = self._max_x_
                 for i in range(2):
                     sys.stdout.write(self.move.LEFT(pos=1000))
                     sys.stdout.write(
                         _input_ + self.c_bg + " " * (self.max_x - (_length_+2) ) + self.init.reset + 
                         self.move.TO(x=self.max_x, y=self.Y) + self.c + f"{self.acs['v']}" +
-                        self.move.TO(x=x, y=self.Y) + self.init.reset + "\n"
-                        )
+                        self.move.TO(x=x, y=self.Y) + self.init.reset + "\n"  )
                     self.Y += 1
-                sys.stdout.write(
-                    self.move.DOWN(1)+
-                    self.move.LEFT(pos=1000)
-                    )
+                sys.stdout.write( self.move.DOWN(1)+ self.move.LEFT(pos=1000) )
                 header.bottom(max_x=self.max_x)
                 sys.stdout.write(self.move.TO(x=x, y=y))
                 sys.stdout.flush()
+                
                 break
-            # moving cursor up, down, left, reight
-            elif self.char == 27:
-                next1, next2 = ord(sys.stdin.read(1)), ord(sys.stdin.read(1))
-                # move left 
-                if   next2 == 68:
-                    if self._x_>= self.length: self._x_ -= 1
-                    else: pass 
-                    
-                    # scroll horizontally string  
-                    if self._x_ == self.length -1: 
-                        if self.counterL > 0: 
-                            self.counterL -= 1
-                            self.counterR -= 1
-                        else: pass
-                    else: pass
-                #move right
-                elif next2 == 67:
-                    if self.counterL == 0:
-                        if self._x_ < ( self.length + self.i-1) : self._x_ += 1
-                        else: pass 
-                    else:    
-                        if self._x_< self.max_x - 3:  self._x_ += 1
-                        else: pass 
-                    
-                    if self._x_ == (self.max_x-2):
-                        # scroll horizontally string  
-                        if self.counterR < len(self.string) : 
-                            self.counterL += 1
-                            self.counterR += 1
-                        else: pass
-                    else: pass 
-                    
-                else: pass
-            # delecting char <backspace>
-            elif self.char in {8, 127}:
-                if self.i > 0: 
-                    self.string = self.string[ : self.i -1] + self.string[self.i : ]
-                    self.i          -= 1
-                    self._x_        -= 1
-                    self.counterL    = 0
-                    self.counterR   -= 1
-                else: pass
-            # another key is pressed 
-            else: 
-                if  self.i < self.max_x - (2 + self.length+1):
-                    if chr(self.char) in case() :
-                        self.string              = self.string[ : self.i] + chr(self.char) + self.string[self.i : ]
-                        self.i                  += 1 
-                        self.counterR            = len(self.string)
-                        self.counterL            = 0
-                        self._x_, self._y_       = screenConfig.cursor()
-                    else: pass
-                else: 
-                    if chr(self.char) in case() :
-                        self.string              = self.string[ : self.i] + chr(self.char) + self.string[self.i : ]
-                        self.i                  += 1 
-                        self.counterL           += 1
-                        self.counterR            = len(self.string)
-                    else: pass
             
-            sys.stdout.write(self.move.LEFT(pos=1000))
-            sys.stdout.write(self.clear.line(2))
-            sys.stdout.write(
-                self.input + self.c_bg + " " * ( self.max_x - (self.length+2) ) + self.init.reset + 
-                self.move.TO(x=self.max_x, y=self._y_) + self.c + f"{self.acs['v']}" +
-                self.move.TO(x=self.length+1, y=self._y_) + self.c_bg+self.string[self.counterL : self.counterR]  + self.init.reset +"\n"
-                )
-            sys.stdout.write(self.clear.screen(0))
-            sys.stdout.write(self.move.LEFT(pos=1000))
-            bottom(max_x=self.max_x)
-            sys.stdout.write(self.move.TO(x=self.length+1, y=self._y_))
-            
-            if self.i > 0:  sys.stdout.write(self.move.RIGHT(pos=(self._x_-self.length)) )
-            else: pass 
-            
-            sys.stdout.flush()
-        
+        #return language type 
         return self.language_type
